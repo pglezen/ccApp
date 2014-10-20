8cd3d504e861f4d35630e3aa4844d10e8efae2c1
Paul Glezen <bs193538@gmail.com>
2014-10-20 10:06:37 -0700

    Use WSSubject instead of UserRegistry for groups.
    Retrieving group information from the JAAS subject obtained from
    WSSubject saves a registry call.

--- CCConsumerWeb/src/org/acme/cc/servlet/CredentialQueryJSON.java
+++ CCConsumerWeb/src/org/acme/cc/servlet/CredentialQueryJSON.java
@@ -1,15 +1,17 @@
 package org.acme.cc.servlet;
 
 import java.io.IOException;
+import java.security.GeneralSecurityException;
 import java.security.Principal;
 import java.util.LinkedHashMap;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-import javax.naming.InitialContext;
-import javax.naming.NamingException;
+import javax.security.auth.Subject;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -18,9 +20,9 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.codehaus.jackson.map.ObjectMapper;
 
-import com.ibm.websphere.security.CustomRegistryException;
-import com.ibm.websphere.security.EntryNotFoundException;
-import com.ibm.websphere.security.UserRegistry;
+import com.ibm.websphere.security.WSSecurityException;
+import com.ibm.websphere.security.auth.WSSubject;
+import com.ibm.websphere.security.cred.WSCredential;
 
 /**
  * Servlet implementation class CredentialQuery.
@@ -57,31 +59,11 @@ public class CredentialQueryJSON extends HttpServlet {
 	      String securityName = userPrincipal.getName();
 	      jsonOutput.put(JSON_SECURITY_NAME, securityName);
 	      log.fine("userPrincipal.getName: " + securityName);
-	      try {
-            InitialContext ic = new InitialContext();
-            UserRegistry registry = (UserRegistry)ic.lookup("UserRegistry");
-            try {
-               String uniqueId = registry.getUniqueUserId(securityName);
-               String displayName = registry.getUserDisplayName(securityName);
-               if (uniqueId != null) {
-                  jsonOutput.put(JSON_UNIQUE_ID, uniqueId);
-               }
-               if (displayName != null) {
-                  jsonOutput.put(JSON_DISPLAY_NAME, displayName);
-               }
-               List<String> groups = registry.getGroupsForUser(securityName);
-               if (groups != null) {
-                  groups.add("BogusGroup1");
-                  groups.add("BogusGroup2");
-                  jsonOutput.put(JSON_GROUPS, groups);
-               }
-            } catch (EntryNotFoundException e) {
-               log.warning("Could not locate security name: " + securityName + " in UserRegistry");
-            } catch (CustomRegistryException e) {
-               log.logp(Level.WARNING, CLASSNAME, "doGet", "Failed to get unique user id", e);
-            }
-         } catch (NamingException e) {
-            log.warning("Failed to retrieve UserRegistry instance from JNDI.");
+	      List<String> groups = getCallerUserGroups();
+         if (groups != null) {
+            groups.add("BogusGroup1");
+            groups.add("BogusGroup2");
+            jsonOutput.put(JSON_GROUPS, groups);
          }
 	      response.setStatus(HttpServletResponse.SC_OK);
 	      response.setContentType("application/json");
@@ -92,5 +74,53 @@ public class CredentialQueryJSON extends HttpServlet {
 	      response.setContentType("application/json");
 	      response.getOutputStream().println("{}");
 	   }
+	}
+	
+	/**
+	 * Return a list of groups for the user ID of the authenticated caller.
+	 * This uses a 
+	 * <a href="http://www.ibm.com/support/knowledgecenter/SSAW57_8.5.5/com.ibm.websphere.javadoc.doc/web/apidocs/com/ibm/websphere/security/auth/WSSubject.html?cp=SSAW57_8.5.5">WAS-specific WSSubject API</a>
+	 * to retrieve group information that was
+	 * already fetched at the initial login time.
+	 */
+	protected static List<String> getCallerUserGroups() {
+	   log.entering(CLASSNAME, "getCallerUserGroups");
+	   List<String> groups = new LinkedList<String>();
+	   try {
+         Subject subject = WSSubject.getCallerSubject();
+         if (subject != null) {
+            Set<WSCredential> creds = subject.getPublicCredentials(WSCredential.class);
+            if (creds.size() == 1) { // We expect exactly one.
+               WSCredential credential = creds.iterator().next();
+               log.fine("        User ID = " + credential.getSecurityName());
+               log.finer("      Unique ID = " + credential.getUniqueSecurityName());
+               log.finer("Realm Unique ID = " + credential.getRealmUniqueSecurityName());
+               log.finer("     Realm Name = " + credential.getRealmName());
+               // This API was fixed before generics came to Java.  It returns an
+               // ArrayList, but can be safely assigned to List<String>.
+               groups = credential.getGroupIds();
+               if (!groups.isEmpty()) {
+                  log.finer("Group membership:");
+                  for (String groupName : groups) {
+                     log.finer("\t" + groupName);
+                  }
+               } else {
+                  log.fine("Group membership is empty.");
+               }
+            } else if (creds.size() == 0) {
+               log.info("WSCredential list is empty.");
+            } else {
+               log.warning("WSCredential list size = " + creds.size() + ". This should not happen.");
+            }
+         } else {
+            log.warning("getCallerSubject returned null; probably unauthenticated.");
+         }
+      } catch (WSSecurityException e) {
+         log.logp(Level.SEVERE, CLASSNAME, "getCallerUserGroups", "Failed to get user groups.", e);
+      } catch (GeneralSecurityException e) {
+         log.logp(Level.SEVERE, CLASSNAME, "getCallerUserGroups", "Failed to get user groups.", e);
+      }
+	   log.exiting(CLASSNAME, "getCallerUserGroups");
+	   return groups;
 	}
 }
